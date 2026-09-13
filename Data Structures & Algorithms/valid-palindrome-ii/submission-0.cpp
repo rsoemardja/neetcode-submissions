e5afa5480284d5a09d3765e9f65a915b4b506d7b
Rama Komarudin Soemardja <rsoemardja@gmail.com>
2026-09-13 13:54:25 +1000

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        if (isPalindrome(s)) {
+            return true;
+        }
+
+        for (int i = 0; i < s.size(); i++) {
+            string newS = s.substr(0, i) + s.substr(i + 1);
+            if (isPalindrome(newS)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+private:
+    bool isPalindrome(const string& s) {
+        int left = 0, right = s.size() - 1;
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
